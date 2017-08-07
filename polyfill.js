9f4ddd32a3715f433916b398dad7142a1c8ea03a
Darien Maillet Valentine <valentinium@gmail.com>
2017-08-06 22:26:17 -0400

    annotate polyfill

--- polyfill.js
+++ polyfill.js
@@ -10,10 +10,17 @@ if (!('fromEntries' in Object)) {
 
     for (const [ index, pair ] of pairs.entries()) {
       if (!pair || typeof pair !== 'object') {
+        // Consistent messaging to Chrome when initializing a Map:
         throw new TypeError(`Iterator value ${ index } is not an entry object`);
       }
 
+      // Consistency with Map: object need not be technically iterable:
+
       const { 0: key, 1: val } = pair;
+
+      // Such that Object.entries(Object.fromEntries(obj)) always yield the
+      // "same" list of entries, symbols are prohibited (along with any other
+      // non-string keys):
 
       if (typeof key !== 'string') {
         throw new TypeError(`Entry object key must be a string`);
